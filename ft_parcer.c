8dbd35109e11071ca311f6c48b3652f47d74c1db
Heidi Merianne <hmeriann@et-f4.kzn.21-school.ru>
2022-03-04 15:32:19 +0300

    start parser

---
+++ ft_parcer.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parcer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmeriann <hmeriann@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/04 14:56:27 by hmeriann          #+#    #+#             */
+/*   Updated: 2022/03/04 15:32:02 by hmeriann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "cub3d.h"
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (ft_str)
+	}
+	else
+		printf("wrong number of arguments\n");
+}
